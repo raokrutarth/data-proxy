64ded983da00ad9bf5f8ec3f59c82168746dcee9
krutarth-rao <krutarth.rao@hpe.com>
2020-11-07 23:30:51 -0800

    code review requests

--- run-proxy.sh
+++ run-proxy.sh
@@ -10,9 +10,10 @@ poetry install
 
 export PYTHONPATH=${PYTHONPATH}:"$(pwd)/app"
 
-poetry run uvicorn \
-    --host 0.0.0.0 \
-    --port ${1:-443} \
-    --log-level info \
-    --workers 4 \
-    main:app
+# poetry run uvicorn \
+#     --host 0.0.0.0 \
+#     --port ${1:-443} \
+#     --log-level info \
+#     --workers 4 \
+#     main:app
+poetry run gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
